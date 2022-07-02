d8c77d132b7745f63452c62cd2f942921d1b05b9
Jay Mevada <jaymevada.dce@gmail.com>
2022-07-02 00:37:44 -0400

    All done. Removed unwanted files

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "2383-assignment1-RG"
-    storage_account_name = "n01522383ssname1"
+    resource_group_name  = "tfstate01522383RG"
+    storage_account_name = "tfstate01522383sa"
     container_name       = "tfstatefiles"
     key                  = "tfstatekey"
   }
